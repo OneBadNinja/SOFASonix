9234350968a940b8ee364adf8a692ebbc4a8d071
I.Laghidze <developer@artisan-one.com>
2020-11-22 15:50:41 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,18 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
   name="SOFASonix",
-  packages=["SOFASonix"],
   version="1.0.7",
-  license='BSD 3-Clause',
-  description="A Lightweight Python API for the AES69-2015 Spatially Oriented Format for Acoustics (SOFA) File Format",
   author='Ioseb Laghidze',
   author_email='developer@artisan-one.com',
+  description="A Lightweight Python API for the AES69-2015 Spatially Oriented Format for Acoustics (SOFA) File Format",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   url='https://github.com/OneBadNinja/SOFASonix/',
-  download_url='https://github.com/OneBadNinja/SOFASonix/archive/1.0.7.tar.gz',
-  keywords=['SOFA', 'Spatially Oriented Format For Acoustics',
-            'Audio', '3D Audio', 'Binaural', 'Transaural', 'Acoustics'],
-  package_data={
-    # Include database file
-    '': ['*.db'],
-    },
-  install_requires=[
-          'netCDF4',
-          'numpy',
-          'pandas',
-      ],
+  packages=setuptools.find_packages(),
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
@@ -36,4 +28,17 @@ setup(
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
   ],
+  python_requires='>=2.7',
+  license='BSD 3-Clause',
+  keywords=['SOFA', 'Spatially Oriented Format For Acoustics',
+            'Audio', '3D Audio', 'Binaural', 'Transaural', 'Acoustics'],
+  package_data={
+    # Include database file
+    'SOFASonix': ['*.db'],
+  },
+  install_requires=[
+          'netCDF4',
+          'numpy',
+          'pandas',
+  ]
 )
